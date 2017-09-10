f60409ee63514b5d1394ac44df2ff720577745ea
fluzzarn <fluzzarn@gmail.com>
2017-09-09 21:38:20 -0400

    Bot Settings Class
    created Bot Settings class that mimics LightBot config.yaml

---
+++ lightbot-net-app/BotSettings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lightbot_net_app
+{
+    class BotSettings
+    {
+        public string Nickname { get; set; }
+        public string OAuthPassword { get; set; }
+        public int Port { get; set; }
+        public string IRCServer { get; set; }
+        public int CheerFloor { get; set; }
+        public int HUEGroup { get; set; }
+        public string Channel { get; set; }
+        public int OffFloor { get; set; }
+        public int OnFloor { get; set; }
+        public AlertSettings AlertSettings { get; set; }
+
+        public BotSettings()
+        {
+
+        }
+    }
+
+    struct AlertSettings
+    {
+        public enum SpecialLightType
+        {
+            None= 0,
+            Blink,
+            Loop,
+
+
+            MAX_LIGHT_TYPES
+        }
+
+        public SpecialLightType Cheer { get; set; }
+        public SpecialLightType Prime { get; set; }
+        public SpecialLightType Sub_1000 { get; set; }
+        public SpecialLightType Sub_2000 { get; set; }
+        public SpecialLightType Sub_3000 { get; set; }
+
+
+    }
+
+}
